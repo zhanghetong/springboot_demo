266a1d1f56a23bad97da7cbdf5886cb14ab93c79
zhanghetong <zhanghe_zht@126.com>
2020-06-13 17:41:09 +0800

    mapperScan

--- src/main/java/com/zht/springboot_mybatis/SpringbootMybatisApplication.java
+++ src/main/java/com/zht/springboot_mybatis/SpringbootMybatisApplication.java
@@ -3,7 +3,6 @@ package com.zht.springboot_mybatis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan(value = "com.zht.springboot_mybatis.mapper")
 @SpringBootApplication
 public class SpringbootMybatisApplication {
 
