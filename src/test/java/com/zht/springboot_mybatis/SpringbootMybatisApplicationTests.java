fb36c05d633c9d26b45d63ce8dc79a1b25f767e2
zhanghetong <zhanghe_zht@126.com>
2020-06-14 10:52:52 +0800

    springboot测试

--- src/test/java/com/zht/springboot_mybatis/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/zht/springboot_mybatis/SpringbootMybatisApplicationTests.java
@@ -1,13 +1,29 @@
 package com.zht.springboot_mybatis;
 
+import com.zht.springboot_mybatis.mapper.first.TestMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootMybatisApplicationTests {
+
+    @Resource
+    TestMapper testMapper;
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void getUser(){
+        int id = 1;
+        System.out.println(testMapper.getUser(id));;
+    };
 
 }
