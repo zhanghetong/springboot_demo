b5d9a85a5bc355b5f9d18795afbc135f714cf4f4
zhanghetong <zhanghetong008@gmail.com>
2020-12-19 21:32:24 +0800

    2

--- src/main/java/com/zht/springboot_mybatis/config/DataSourceMysqlConfig.java
+++ src/main/java/com/zht/springboot_mybatis/config/DataSourceMysqlConfig.java
@@ -1,7 +1,6 @@
 package com.zht.springboot_mybatis.config;
 
 
-
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
